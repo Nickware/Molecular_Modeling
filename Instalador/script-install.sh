acc42d3199f08bc6cd0c998123f751665eca2005
Nicolas Torres <nickthor@dm3-1148la>
2016-10-24 11:28:36 -0500

    Actualizacion Cursillo

--- Instalador/script-install.sh
+++ Instalador/script-install.sh
@@ -6,20 +6,11 @@
 #Arquitectura x86
 sudo apt-get -y install build-essential fftw3-dev gfortran
 sudo apt-get -y install liblapack-dev fftw-dev
-wget http://qe-forge.org/gf/download/frsrelease/211/968/espresso-5.4.0.tar.gz
-tar -xvf espresso-5.4.0.tar.gz
-cd espresso-5.4.0
-sudo ./configure
-sudo make all
-cd ..
-mv espresso-5.4.0 espresso/
-sudo mv espresso/ /opt/
-export PATH=$PATH:/opt/espresso/bin/
-source ~/.bashrc
+sudo apt-get -y install libopenmpi-dev
 sudo apt-get -y install openmpi
 sudo apt-get -y install openbabel
 sudo apt-get -y install pymol
-wget https://launchpad.net/ubuntu/+archive/primary/+files/nwchem_6.6+r27746-2_amd64.deb
+wget https://launchpad.net/ubuntu/+archive/primary/+files/nwchem_6.5+r26243-6_amd64.deb
 sudo dpkg -i nwchem_6.6+r27746-2_amd64.deb
 wget http://www.ks.uiuc.edu/Research/vmd/vmd-1.9.3/files/beta3/vmd-1.9.3beta3.bin.LINUXAMD64-OptiX.opengl.tar.gz
 tar -xvf vmd-1.9.3beta3.bin.LINUXAMD64-OptiX.opengl.tar.gz
@@ -27,3 +18,13 @@ mv vmd-1.9.3beta3/ vmd/
 sudo mv vmd/ /usr/local/bin
 cd /usr/local/bin/vmd
 sudo ./configure
+#wget http://qe-forge.org/gf/download/frsrelease/211/968/espresso-5.4.0.tar.gz
+#tar -xvf espresso-5.4.0.tar.gz
+#cd espresso-5.4.0
+#sudo ./configure
+#sudo make all
+#cd ..
+#mv espresso-5.4.0 espresso/
+#sudo mv espresso/ /opt/
+#export PATH=$PATH:/opt/espresso/bin/
+#source ~/.bashrc
