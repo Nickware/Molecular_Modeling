3036c615fc505c9573f23adc78b5d6e3e77f6771
Nicolás Torres <naturalezacomputacional@gmail.com>
2025-04-20 21:03:21 -0500

    Update complements-build.sh. Improving the installer logic.

--- installation_routines/complements-build.sh
+++ installation_routines/complements-build.sh
@@ -1,25 +1,85 @@
 #!/bin/bash
-#Scritp para la construccion de complementos
-#Version 1.0
-#Date: 05/04/2025
-#Distribuciones Debian y derivadas.
-#Arquitectura x86
-echo "Verificar version de la distribucion"
-sudo apt-get update
-#wget http://www.ks.uiuc.edu/Research/vmd/vmd-1.9.1/files/final/vmd-1.9.1.bin.LINUXAMD64.opengl.tar.gz
-#tar -zxvf vmd-1.9.1.bin.LINUXAMD64.opengl.tar.gz
-#cd vmd-1.9.1
-#./configure LINUXAMD64
-#./configure
-#cd src
-#make install
-#wget http://qe-forge.org/gf/download/frsrelease/211/968/espresso-5.4.0.tar.gz
-#tar -xvf espresso-5.4.0.tar.gz
-#cd espresso-5.4.0
-#sudo ./configure
-#sudo make all
-#cd ..
-#mv espresso-5.4.0 espresso/
-#sudo mv espresso/ /opt/
-#export PATH=$PATH:/opt/espresso/bin/
-#source ~/.bashrc
+#============================================================
+# Script de instalación automática de herramientas científicas VMD y Quantum Espresso
+# Versión: 1.0
+# Fecha: 05/04/2025
+# Compatible con: Distribuciones basadas en Debian (x86_64)
+# Herramientas: VMD 1.9.1 y Quantum ESPRESSO 5.4.0
+#============================================================
+
+# ------------------------------
+# Parámetros configurables
+# ------------------------------
+VMD_VERSION="1.9.1"
+QE_VERSION="5.4.0"
+INSTALL_DIR="/opt"
+USER_HOME="/home/$SUDO_USER"
+
+# ------------------------------
+# Verificación del usurio root
+# ------------------------------
+if [[ $EUID -ne 0 ]]; then
+  echo "Este script debe ejecutarse como root. Usa: sudo $0"
+  exit 1
+fi
+
+# ------------------------------
+# ACTUALIZACIÓN DE REPOSITORIOS
+# ------------------------------
+echo " Actualizando repositorios de paquetes..."
+apt-get update -y
+
+# ------------------------------
+# Instalación de dependecias básicas
+# ------------------------------
+echo " Instalando dependencias requeridas para compilar..."
+apt-get install -y build-essential gfortran wget tar \
+  libfftw3-dev libblas-dev liblapack-dev libxc-dev \
+  libopenmpi-dev openmpi-bin xauth libglu1-mesa
+
+# ------------------------------
+# Instalación VMD (decargar, construir e instalar)
+# ------------------------------
+echo " Descargando VMD $VMD_VERSION..."
+cd /tmp
+wget -c http://www.ks.uiuc.edu/Research/vmd/vmd-$VMD_VERSION/files/final/vmd-$VMD_VERSION.bin.LINUXAMD64.opengl.tar.gz
+
+echo " Extrayendo VMD..."
+tar -xzf vmd-$VMD_VERSION.bin.LINUXAMD64.opengl.tar.gz
+cd vmd-$VMD_VERSION
+./configure LINUXAMD64
+cd src
+make install
+cd /tmp
+
+# ------------------------------
+# Instalación Quantum Espresso (descargar, construir e instalar)
+# ------------------------------
+echo " Descargando Quantum ESPRESSO $QE_VERSION..."
+wget -c http://qe-forge.org/gf/download/frsrelease/211/968/espresso-$QE_VERSION.tar.gz
+
+echo " Extrayendo Quantum ESPRESSO..."
+tar -xvf espresso-$QE_VERSION.tar.gz
+mv espresso-$QE_VERSION $INSTALL_DIR/espresso
+
+echo " Compilando Quantum ESPRESSO..."
+cd $INSTALL_DIR/espresso
+./configure
+make all
+
+# ------------------------------
+# Configuración del entorno
+# ------------------------------
+echo " Configurando PATH para Quantum ESPRESSO..."
+QE_PATH_LINE="export PATH=\$PATH:$INSTALL_DIR/espresso/bin"
+if ! grep -q "$QE_PATH_LINE" "$USER_HOME/.bashrc"; then
+  echo "$QE_PATH_LINE" >> "$USER_HOME/.bashrc"
+  echo " PATH de Quantum ESPRESSO agregado a ~/.bashrc"
+fi
+
+# ------------------------------
+# Finalización
+# ------------------------------
+echo " Instalación completada exitosamente."
+echo " Reinicia tu terminal o ejecuta: source ~/.bashrc"
+
