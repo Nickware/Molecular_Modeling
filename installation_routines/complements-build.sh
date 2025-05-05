eb6aca63b56c4a33a97111ab3ab84c305bd79a60
Nicolás Torres <naturalezacomputacional@gmail.com>
2025-05-04 22:19:22 -0500

    Update complements-build.sh

--- installation_routines/complements-build.sh
+++ installation_routines/complements-build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# Script de instalación de VMD y Quantum ESPRESSO
+# Script para construir VMD y Quantum ESPRESSO
 # Versión 1.3 – Fecha: 20/04/2025
 # Para distribuciones Debian y derivadas – Arquitectura x86_64
 # Autor: N.Torres
-# Descripción: Este script instala VMD y Quantum ESPRESSO en sistemas Debian y derivados.
 # Requiere privilegios de superusuario para instalar paquetes y modificar el PATH.
 # Uso: Ejecutar el script como superusuario o con sudo.
-# Asegúrate de tener privilegios de superusuario para ejecutar este script.
 # Este script ha sido probado en Zorin 17.04 - Ubuntu 24.04 - Debian 12
 
 set -e  # Detiene ejecución si ocurre algún error
